06ff51a129afa6ecc4f924cdddc38cdefe8de3d9
cbt-devs <cbt.olongapo@gmail.com>
2025-06-01 19:24:06 +0800

    page AJAX bug fix

--- modules/footer.php
+++ modules/footer.php
@@ -1,2 +1,27 @@
 <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
 <script src="assets/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="https://cdn.datatables.net/2.2.2/js/dataTables.js"></script>
+<script src="https://cdn.datatables.net/2.2.2/js/dataTables.bootstrap5.js"></script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/2.2.2/css/dataTables.bootstrap5.css" rel="stylesheet">
+
+<script>
+$(document).ready(function() {
+    // Handle dynamic page loading
+    $('.load-content').on('click', function(e) {
+        e.preventDefault();
+
+        var page = $(this).data('page');
+
+        $.get(page, function(data) {
+            $('.contents').html(data);
+
+            // Re-initialize DataTable if the table exists in loaded content
+            if ($('#example').length) {
+                $('#example').DataTable();
+            }
+        });
+    });
+});
+</script>
--- modules/header.php
+++ modules/header.php
@@ -14,11 +14,8 @@
     <link href="assets/css/main.css" rel="stylesheet">
 
     <link href="assets/js/datatable.js" rel="stylesheet">
-    
-<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.datatables.net/2.2.2/js/dataTables.js"></script>
-<script src="https://cdn.datatables.net/2.2.2/js/dataTables.bootstrap5.js"></script>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
     <script src="assets/chartjs/dist/chart.umd.js"></script>
 
