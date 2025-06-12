f84c358440c2630cb63788a9d13d5f3fcf239ac3
cbt-devs <cbt.olongapo@gmail.com>
2025-06-12 13:37:05 +0800

    dyanmic member, event and newly baptist count

--- modules/main.php
+++ modules/main.php
@@ -1,3 +1,33 @@
+<?php
+    require_once 'class/database.php';
+    require_once 'class/member.php';
+    require_once 'class/ministries.php';
+    require_once 'class/events.php';
+
+    $pdo = new Database();
+    $conn = $pdo->getConnection();
+
+    $member = new Member($conn);
+    $ministry = new Ministries($conn);
+    $event = new Events($conn);
+
+    $event_ctr = count( $event->show() );
+    $member_ctr = count( $member_r = $member->show( 1 ) );
+
+    $newly_baptist_ctr = 0;
+    foreach( $member_r as $member ) {
+        if( $member[ 'baptism_date' ] ?? 0 ) {
+            $baptismDate = new DateTime($member['baptism_date']);
+            $currentDate = new DateTime();
+
+            if( $baptismDate->format('m') === $currentDate->format('m') &&
+                $baptismDate->format('Y') === $currentDate->format('Y') ) {
+                    $newly_baptist_ctr += 1;
+            }
+        }
+    }
+?>
+
 <div class="row">
     <div class="card col-2 m-2">
         <div class="card-body d-flex flex-column flex-wrap align-content-center">
@@ -8,7 +38,7 @@
     <div class="card col-2 m-2">
         <div class="card-body d-flex flex-column flex-wrap align-content-center">
             <i class="fa-solid fa-user-group fa-2x text-primary"></i>
-            7 total members
+            <?= $member_ctr ?> total members
         </div>
     </div>
     <div class="card col-2 m-2">
@@ -20,13 +50,13 @@
     <div class="card col-2 m-2">
         <div class="card-body d-flex flex-column flex-wrap align-content-center">
             <i class="fa-solid fa-water-ladder fa-2x text-primary"></i>
-            5 newly baptist
+            <?=  $newly_baptist_ctr ?> newly baptist
         </div>
     </div>
     <div class="card col-2 m-2">
         <div class="card-body d-flex flex-column flex-wrap align-content-center">
             <i class="fa-solid fa-calendar-check fa-2x text-primary"></i>
-            3 events
+            <?= $event_ctr ?> events
         </div>
     </div>
 </div>
