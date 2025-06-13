8801effce8f075d9d9cb63a565d3dad67fee5d04
cbt-devs <cbt.olongapo@gmail.com>
2025-06-14 00:39:49 +0800

    Added logo

--- modules/navbar.php
+++ modules/navbar.php
@@ -3,73 +3,72 @@ $nav = new Navigation();
 ?>
 
 <nav class="col-md-2 p-3 d-flex flex-column shadow-sm">
-    <h3 class="text-center">CBT</h5>
+    <img src="assets/img/CBT Logo 1.svg" alt="CBT Logo" width="100" class="mx-auto d-block">
 
-        <ul class="nav flex-column flex-grow-1">
-            <li class="nav-item bg-info-subtle rounded">
-                <a href="#" class="nav-link load-content hover-bg-light" data-page="<?= $nav->getLink('main') ?>">
-                    <i class="fa-solid fa-chart-simple me-2"></i>Dashboard
-                </a>
-            </li>
+    <ul class="nav flex-column flex-grow-1 mt-3">
+        <li class="nav-item bg-info-subtle rounded">
+            <a href="#" class="nav-link load-content hover-bg-light" data-page="<?= $nav->getLink('main') ?>">
+                <i class="fa-solid fa-chart-simple me-2"></i>Dashboard
+            </a>
+        </li>
 
-            <!-- Management Dropdown -->
-            <li class="nav-item dropdown">
-                <a href="#" class="nav-link d-flex justify-content-between align-items-center" data-bs-toggle="collapse"
-                    data-bs-target="#managementDropdown" aria-expanded="false" aria-controls="managementDropdown">
-                    <span><i class="fa-solid fa-table me-2"></i>Management</span>
-                    <i class="fa-solid fa-chevron-down"></i>
-                </a>
-                <div class="collapse" id="managementDropdown">
-                    <ul class="nav flex-column ms-3">
-                        <li class="nav-item">
-                            <a href="#" class="nav-link load-content"
-                                data-page="<?= $nav->getLink('missions') ?>">Missions</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link load-content"
-                                data-page="<?= $nav->getLink('members') ?>">Members</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link load-content"
-                                data-page="<?= $nav->getLink('events') ?>">Events</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link load-content"
-                                data-page="<?= $nav->getLink('ministries') ?>">Ministries</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link load-content"
-                                data-page="<?= $nav->getLink('commitments') ?>">Commitments</a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
+        <!-- Management Dropdown -->
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link d-flex justify-content-between align-items-center" data-bs-toggle="collapse"
+                data-bs-target="#managementDropdown" aria-expanded="false" aria-controls="managementDropdown">
+                <span><i class="fa-solid fa-table me-2"></i>Management</span>
+                <i class="fa-solid fa-chevron-down"></i>
+            </a>
+            <div class="collapse" id="managementDropdown">
+                <ul class="nav flex-column ms-3">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link load-content"
+                            data-page="<?= $nav->getLink('missions') ?>">Missions</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link load-content"
+                            data-page="<?= $nav->getLink('members') ?>">Members</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link load-content" data-page="<?= $nav->getLink('events') ?>">Events</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link load-content"
+                            data-page="<?= $nav->getLink('ministries') ?>">Ministries</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link load-content"
+                            data-page="<?= $nav->getLink('commitments') ?>">Commitments</a>
+                    </li>
+                </ul>
+            </div>
+        </li>
 
-            <!-- Tools Dropdown -->
-            <li class="nav-item dropdown">
-                <a href="#" class="nav-link d-flex justify-content-between align-items-center" data-bs-toggle="collapse"
-                    data-bs-target="#toolsDropdown" aria-expanded="false" aria-controls="toolsDropdown">
-                    <span><i class="fa-solid fa-screwdriver-wrench me-2"></i>Tools</span>
-                    <i class="fa-solid fa-chevron-down"></i>
-                </a>
-                <div class="collapse" id="toolsDropdown">
-                    <ul class="nav flex-column ms-3">
-                        <li class="nav-item">
-                            <a href="#" class="nav-link load-content"
-                                data-page="<?= $nav->getLink('pp_generator') ?>">Create PowerPoint</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link load-content"
-                                data-page="<?= $nav->getLink('schedule') ?>">Schedule</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link load-content"
-                                data-page="<?= $nav->getLink('documentation') ?>">Documentation</a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-        </ul>
+        <!-- Tools Dropdown -->
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link d-flex justify-content-between align-items-center" data-bs-toggle="collapse"
+                data-bs-target="#toolsDropdown" aria-expanded="false" aria-controls="toolsDropdown">
+                <span><i class="fa-solid fa-screwdriver-wrench me-2"></i>Tools</span>
+                <i class="fa-solid fa-chevron-down"></i>
+            </a>
+            <div class="collapse" id="toolsDropdown">
+                <ul class="nav flex-column ms-3">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link load-content"
+                            data-page="<?= $nav->getLink('pp_generator') ?>">Create PowerPoint</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link load-content"
+                            data-page="<?= $nav->getLink('schedule') ?>">Schedule</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link load-content"
+                            data-page="<?= $nav->getLink('documentation') ?>">Documentation</a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+    </ul>
 
 
 </nav>
