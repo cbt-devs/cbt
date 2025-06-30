d5c218aa1e860c5dfa4e366defd1b93ec91fdd49
jjacobo-00 <jacobojhay13@gmail.com>
2025-06-30 15:05:01 +0800

    added function check_access

--- class/accounts.php
+++ class/accounts.php
@@ -14,4 +14,27 @@ class Accounts
     {
         $this->conn = $db;
     }
+
+    public function check_access(int $accounts_id): bool
+    {
+        if ($accounts_id <= 0) {
+            return false;
+        }
+
+        $stmt = $this->conn->prepare("SELECT role_id FROM accounts_access WHERE accounts_id = ?");
+        $stmt->execute([$accounts_id]);
+
+        $role_id = $stmt->fetchColumn();
+
+        if (!$role_id) {
+            return false;
+        }
+
+        return self::$access_rights[(int)$role_id] === 'admin';
+    }
+
+    public static function getRoleName(int $role_id): ?string
+    {
+        return self::$access_rights[$role_id] ?? null;
+    }
 }
