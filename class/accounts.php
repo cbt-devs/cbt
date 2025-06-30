b902e3a9a34d96f7557a6f68c77ace1d2c3d199e
jjacobo-00 <jacobojhay13@gmail.com>
2025-06-30 13:36:14 +0800

    added access rights

--- class/accounts.php
+++ class/accounts.php
@@ -3,6 +3,13 @@ class Accounts
 {
     private $conn;
 
+    private static array $access_rights = [
+        1 => 'admin',
+        2 => 'leader',
+        3 => 'member',
+        4 => 'visitor'
+    ];
+
     public function __construct($db)
     {
         $this->conn = $db;
