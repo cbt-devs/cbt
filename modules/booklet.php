674f254bd483d63730bceddd30399c691afd073a
jjacobo-00 <jacobojhay13@gmail.com>
2025-08-22 21:53:18 +0800

    added booklet file

---
+++ modules/booklet.php
@@ -0,0 +1,9 @@
+<div class="d-flex justify-content-between align-items-start">
+    <div>
+        <h2>Booklet Management</h2>
+        <p>Manage and edit the content of your booklets here.</p>
+    </div>
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addModal">
+        <i class="fa-solid fa-plus"></i> Create booklet
+    </button>
+</div>
